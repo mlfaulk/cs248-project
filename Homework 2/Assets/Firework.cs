0fa485bcceb7a3daf6ba2a319939aa9bec1f675b
mlfaulk <Megan@Megans-Air.attlocal.net>
2016-01-24 18:04:54 -0800

    added content

---
+++ Homework 2/Assets/Firework.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Firework : MonoBehaviour {
+
+	public GameObject flurry_prefab;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			var v3 = Input.mousePosition;
+			v3.z = 10;
+			v3 = Camera.main.ScreenToWorldPoint(v3);
+			Instantiate (flurry_prefab, v3, transform.rotation);
+		}
+	}
+}
---
+++ Homework 2/Assets/Follow.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Follow : MonoBehaviour {
+
+	public GameObject flurry_prefab;
+
+	// Use this for initialization
+	void Start () {
+		var v3 = Input.mousePosition;
+		v3.z = 10;
+		v3 = Camera.main.ScreenToWorldPoint(v3);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		var v3 = Input.mousePosition;
+		v3.z = 10;
+		v3 = Camera.main.ScreenToWorldPoint(v3);
+		//flurry_prefab.transform.position = v3;
+		Instantiate (flurry_prefab, v3, transform.rotation);
+	}
+}
